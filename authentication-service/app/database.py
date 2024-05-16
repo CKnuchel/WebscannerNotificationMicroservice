b14dad2b9e2143876e0600478fe7a18370eb18bd
CKuchel <christoph.knuchel@gmail.com>
2024-05-16 19:39:56 +0200

    ADDED: Auth DB and User

--- authentication-service/app/database.py
+++ authentication-service/app/database.py
@@ -1,25 +1,18 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
+import os
 
-# Load environment variables from .env file
-load_dotenv()
-
-# Retrieve environment variables
-MYSQL_USER = os.getenv("MYSQL_USER")
-MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
-MYSQL_DB = os.getenv("MYSQL_DB")
+MYSQL_USER = os.getenv("AUTH_USER")
+MYSQL_PASSWORD = os.getenv("AUTH_PASSWORD")
 MYSQL_HOST = os.getenv("MYSQL_HOST")
 MYSQL_PORT = os.getenv("MYSQL_PORT")
+MYSQL_DB = os.getenv("MYSQL_AUTH_DB")
 
-# Construct the SQLAlchemy database URL
 SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
 def get_db():
--- database/init.sql
+++ database/init.sql
@@ -1,7 +1,17 @@
------------------------------------------------------------------------------------------------------------------------
--- Information
------------------------------------------------------------------------------------------------------------------------
--- This file is used to initialize the database with the required tables and data.
--- It is executed when the database is created for the first time.
------------------------------------------------------------------------------------------------------------------------
-USE `mydatabase`;
+-- Auth DB
+CREATE DATABASE IF NOT EXISTS auth_db;
+
+-- Create and grant privileges to the auth user
+CREATE USER IF NOT EXISTS 'auth_api'@'%' IDENTIFIED WITH 'caching_sha2_password' BY 'place_a_guid';
+GRANT ALL PRIVILEGES ON auth_db.* TO 'auth_api'@'%';
+FLUSH PRIVILEGES;
+
+USE auth_db;
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    hashed_password VARCHAR(100) NOT NULL,
+    role VARCHAR(50) DEFAULT 'user'
+);
+
+-- other DB's
