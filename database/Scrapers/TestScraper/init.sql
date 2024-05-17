375d66457d879016b756ea79898b7694ade08f39
CKuchel <christoph.knuchel@gmail.com>
2024-05-17 21:24:27 +0200

    CHANGED: Removed Comments to fix "Syntax Error" Message on Containerstartup

--- database/Scrapers/TestScraper/init.sql
+++ database/Scrapers/TestScraper/init.sql
@@ -1,8 +1,5 @@
--- Initialize the database
 CREATE DATABASE IF NOT EXISTS test_scraper_db;
 
--- Grant permissions to the testscraperuser from any host
 GRANT ALL PRIVILEGES ON auth_db.* TO 'testscraperuser'@'%' IDENTIFIED BY 'testscraperpassword';
 
--- Apply the changes
 FLUSH PRIVILEGES;
--- database/auth/init.sql
+++ database/auth/init.sql
@@ -1,8 +1,5 @@
--- Initialize the database
 CREATE DATABASE IF NOT EXISTS auth_db;
 
--- Grant permissions to the authuser from any host
 GRANT ALL PRIVILEGES ON auth_db.* TO 'authuser'@'%' IDENTIFIED BY 'authpassword';
 
--- Apply the changes
 FLUSH PRIVILEGES;
