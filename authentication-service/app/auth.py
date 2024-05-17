2403a4d03047c233909b96aeda6178e9bc223bda
CKuchel <christoph.knuchel@gmail.com>
2024-05-17 17:24:32 +0200

    ADDED: validate-token Endpoint

--- authentication-service/app/auth.py
+++ authentication-service/app/auth.py
@@ -13,17 +13,19 @@ from .utils import verify_password
 router = APIRouter()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = "HS256"
+ALGORITHM = os.getenv("TOKEN_ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
+# Function to authenticate the user
 def authenticate_user(db, username: str, password: str):
     user = get_user(db, username)
     if not user or not verify_password(password, user.hashed_password):
         return False
     return user
 
+# Function to create the access token
 def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
     if expires_delta:
@@ -34,6 +36,7 @@ def create_access_token(data: dict, expires_delta: timedelta = None):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
+# Endpoint to get the token
 @router.post("/token", response_model=Token)
 async def login_for_access_token(db: Session = Depends(get_db), form_data: OAuth2PasswordRequestForm = Depends()):
     user = authenticate_user(db, form_data.username, form_data.password)
@@ -47,6 +50,7 @@ async def login_for_access_token(db: Session = Depends(get_db), form_data: OAuth
     access_token = create_access_token(data={"sub": user.username, "role": user.role}, expires_delta=access_token_expires)
     return {"access_token": access_token, "token_type": "bearer"}
 
+# Endpoint to create a new user
 @router.post("/users/", response_model=UserResponse)
 def create_new_user(user: UserCreate, db: Session = Depends(get_db)):
     db_user = get_user(db, user.username)
@@ -54,3 +58,13 @@ def create_new_user(user: UserCreate, db: Session = Depends(get_db)):
         raise HTTPException(status_code=400, detail="Username already registered")
     new_user = create_user(db=db, user=user)
     return new_user
+
+# Endpoint to Validate the token
+# Other services can use this endpoint to validate the token
+@router.get("/validate-token")
+async def validate_token(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return {"valid": True, "username": payload.get("sub"), "role": payload.get("role")}
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
