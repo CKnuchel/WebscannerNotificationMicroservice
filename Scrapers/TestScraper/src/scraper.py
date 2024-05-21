9c8fcd2c8dd03a0df5f6fb7a4acb83e5a5f43127
CKuchel <christoph.knuchel@gmail.com>
2024-05-21 18:37:08 +0200

    DEFINED Scraper

--- Scrapers/TestScraper/src/scraper.py
+++ Scrapers/TestScraper/src/scraper.py
@@ -1,10 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 from sqlalchemy.orm import Session
-from database import SessionLocal, get_db
-from crud import create_category, get_category_by_name
-from schemas import CategoryCreate
-from models import Category
+from crud import create_category, get_category_by_name, create_product, create_product_detail
+from schemas import CategoryCreate, ProductCreate, ProductDetailCreate
+from models import Category, Product
 
 BASE_URL = "https://webscraper.io/"
 TEST_SITE_URL = "/test-sites/e-commerce/static"
@@ -61,17 +60,51 @@ def scrape_products(db: Session,):
     # Get all the subcategories, because they contain all the products
     categories = db.query(Category).filter(Category.level == "sub").all()
 
-    products_to_create = []
-
     for category in categories:
+        if not category:
+            continue
+
         response = requests.get(category.url)
         soup = BeautifulSoup(response.text, "html.parser")
 
         products = soup.find_all("div", class_="col-md-4 col-xl-4 col-lg-4")
-        products_to_create.extend(products) # Add all the products to the list
+        
+        for product in products:
+            product_name = product.find("a", class_="title").text.strip()
+            product_category_id = category.id
+            product_url = BASE_URL + product.find("a", class_="title")["href"]
+
+            # Check if the product already exists in the database
+            if not get_product_by_name(db, product_name):
+                create_product(db, ProductCreate(name=product_name, category_id=product_category_id, url=product_url))
+
 
         # Go to the next page if it exists
+        next_page = soup.find("a", rel="next").get("href", None)
+        if next_page:
+            next_page_url = BASE_URL + next_page
+            scrape_products(db, next_page_url)
+          
 
-        # scrape the products and add them to the list
+def scrape_product_details(db: Session):
+    products = db.query(Product).all()
 
-    # Create the products in the database
+    for product in products:
+        response = requests.get(product.url)
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        card = soup.find("div", class_="card")
+        product_thumbnail = card.find("img", class_="img-responsive")["src"]
+        product_description = card.find("p", class_="description").text.strip()
+        product_price = card.find("h4", class_="price").text.strip()
+        product_options = card.find_all("div", class_="swatches").find_all("button").text.strip()
+        product_reviews = card.find("p", class_="review-count").text.split(' ')[0]
+
+        # Create the product detail in the database, if it does not exist
+        create_product_detail(db, ProductDetailCreate(
+            product_id=product.id, 
+            thumbnail=product_thumbnail, 
+            description=product_description, 
+            price=product_price, 
+            options=product_options, 
+            reviews_count=product_reviews))
